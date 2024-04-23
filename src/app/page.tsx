286525ee5f1b0dca618a1cadae42055bfa977563
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-04-23 00:24:53 -0500

    Feat: Added gradient animation to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import MyThings from '@/components/my-things';
 
 export default function Home() {
 	return (
-		<div className='flex max-h-full flex-col items-center justify-between'>
-			<div className='z-10 rounded-xl border bg-background p-8 shadow-md'>
-				<h1 className='text-4xl font-semibold'>
+		<div className='flex max-h-full flex-col items-center justify-between p-12'>
+			<div className='z-10 rounded-xl border bg-background p-6 shadow-md sm:p-8'>
+				<h1 className='text-2xl font-semibold sm:text-4xl'>
 					Hi, I&apos;m Aldi, a{' '}
-					<span className='bg-gradient-to-br from-red-500 from-10% via-purple-700 to-green-500 bg-clip-text text-transparent'>
+					<span className='animate-gradient-slide block bg-gradient-to-br from-red-500 from-10% via-purple-700 via-50% to-green-500 to-90% bg-clip-text text-3xl text-transparent sm:inline sm:text-4xl'>
 						web developer
 					</span>
 				</h1>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -77,6 +77,7 @@ const config = {
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
+				'gradient-slide': 'gradient-slide 6s ease-in-out infinite',
 			},
 		},
 	},
